07a55906dcee3ca17ce60e5124869db90dcc76ed
ALI RAZA <razadeveloper816@gmail.com>
2025-12-22 21:54:01 -0800

    complete heroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './Component/Header/Header';
+import HeroSection from './Component/HeroSection/HeroSection';
 
 function App() {
 
   return (
    <div className='container'>
     <Header/>
+    <main>
+      <HeroSection/>
+    </main>
    </div>
   )
 }
---
+++ src/Component/HeroSection/HeroSection.jsx
@@ -0,0 +1,21 @@
+import style from '../../Css/HeroSection.module.css';
+
+export default function HeroSection() {
+    return (
+        <div className={style.heroContainer}>
+            <div className={style.heroContent}>
+                <div>
+                    <img src="/group_profile.png" alt="" />
+                    <span>Trusted 10k+ people</span>
+                </div>
+                <div>
+                    <h1>Turning imagination into <br /> <span>digital</span> impact.</h1>
+                    <p>Creating meaningful connections and turning big ideas <br /> into interactive digital experiences.</p>
+                </div>
+                <div className={style.heroImage}>
+                    <img src="/hero_img.png" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
