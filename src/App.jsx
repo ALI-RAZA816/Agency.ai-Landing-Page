3b7908edfabc5617373c30937a108a98700b9397
ALI RAZA <razadeveloper816@gmail.com>
2025-12-23 09:25:08 -0800

    work component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Companies from './Component/Companies/Companies';
 import Header from './Component/Header/Header';
 import HeroSection from './Component/HeroSection/HeroSection';
+import LatestWork from './Component/Latestwork/LatestWork';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <main>
         <HeroSection />
         <Companies />
+        <LatestWork/>
       </main>
     </>
   )
---
+++ src/Component/Latestwork/LatestWork.jsx
@@ -0,0 +1,35 @@
+import style from '../../Css/LatestWork.module.css';
+
+export default function LatestWork() {
+    return (
+        <div className={style.latestWork}>
+            <div className={style.content}>
+                <h2>Out latest work</h2>
+                <p>Browse our portfolio of innovative digital projects that showcase <br /> creativity, performance, and results.</p>
+                <div className={style.work}>
+                    <div className={style.workBox}>
+                        <div>
+                            <img src="/work_mobile_app.png" alt="" />
+                        </div>
+                        <span>Mobile app marketing</span>
+                        <p>We turn bold ideas into powerful digital solutions that connect...</p>
+                    </div>
+                    <div className={style.workBox}>
+                        <div>
+                            <img src="/work_dashboard_management.png" alt="" />
+                        </div>
+                        <span>Dashboard management</span>
+                        <p>We turn bold ideas into powerful digital solutions that connect...</p>
+                    </div>
+                    <div className={style.workBox}>
+                        <div>
+                            <img src="/work_fitness_app.png" alt="" />
+                        </div>
+                        <span>Fitness app promation</span>
+                        <p>We turn bold ideas into powerful digital solutions that connect...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
