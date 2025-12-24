218ddbb30fac7729e3b446a3dcdd0bf3bf592932
ALI RAZA <razadeveloper816@gmail.com>
2025-12-24 08:50:08 -0800

    apply responsiveness on footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Companies from './Component/Companies/Companies';
 import Contact from './Component/Contact/Contact';
+import { ContextValues } from './Component/Context/Context';
 import Footer from './Component/Footer/Footer';
 import Header from './Component/Header/Header';
 import HeroSection from './Component/HeroSection/HeroSection';
@@ -9,21 +10,24 @@ import Team from './Component/Team/Team';
 
 function App() {
 
+
   return (
     <>
-      <header>
-        <Header />
-      </header>
-      <main>
-        <HeroSection />
-        <Companies />
-        <LatestWork/>
-        <Team/>
-        <Contact/>
-      </main>
-      <footer>
-        <Footer/>
-      </footer>
+      <ContextValues.Provider>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <HeroSection />
+          <Companies />
+          <LatestWork />
+          <Team />
+          <Contact />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </ContextValues.Provider>
     </>
   )
 }
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,20 +2,22 @@ import { LuMoon } from "react-icons/lu";
 import { HiArrowLongRight } from "react-icons/hi2";
 import style from '../../Css/Header.module.css';
 
+
 export default function Header() {
+
   return (
     <div className={style.header}>
       <div className={style.companyLogo}>
         <img src="/Group 1.png" alt="" /><img src="/InsiderStats.png" alt="" />
       </div>
-      <div className={style.link}>
+      {/* <div className={style.link}>
         <ul>
             <li><a href="">Home</a></li>
             <li><a href="">Services</a></li>
             <li><a href="">Our Work</a></li>
             <li><a href="">Testimonials</a></li>
         </ul>
-      </div>
+      </div> */}
       <div className={style.contact}>
         <div className={style.mode}><LuMoon className={style.darkMode} /></div>
         <button><span>Connect</span><HiArrowLongRight className={style.arrow} /></button>
